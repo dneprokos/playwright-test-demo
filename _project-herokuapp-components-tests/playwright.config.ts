6e4c8ed3915010cd39bee02b4ea7aa202dc9f1bf
Kostiantyn Teltov <dneprokos@gmail.com>
2024-01-14 12:26:38 +0100

    Fixed result folder name

--- _project-herokuapp-components-tests/playwright.config.ts
+++ _project-herokuapp-components-tests/playwright.config.ts
@@ -13,7 +13,7 @@ const config: PlaywrightTestConfig = {
     workers: getEnvParseNumber(EnvironmentParameters.workers)?? 4,
     //Reporters for the test results
     reporter: [
-        ['junit', { outputFile: 'test-result/results.xml' }],
+        ['junit', { outputFile: 'test-results/results.xml' }],
         ['allure-playwright'],
         ['list']
     ],
