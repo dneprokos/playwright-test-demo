aeedb6593dd044ca11cdcf0e2290bb414b424dab
Kostiantyn Teltov <dneprokos@gmail.com>
2025-02-09 18:14:47 +0100

    Added a few test step examples

--- _project-herokuapp-components-tests/pages/login-page/login-page.ts
+++ _project-herokuapp-components-tests/pages/login-page/login-page.ts
@@ -1,47 +1,65 @@
 import { BasePage } from "@framework/page-base/base-page";
-import { Locator, Page } from "@playwright/test";
+import test, { expect, Locator, Page } from "@playwright/test";
 
 export default class LoginPage extends BasePage {
-    userNameLocator: Locator;
-    passwordLocator: Locator;
-    loginFormLocator: Locator;
-    loginTextContentLocator: Locator;
+  userNameLocator: Locator;
+  passwordLocator: Locator;
+  loginFormLocator: Locator;
+  loginTextContentLocator: Locator;
 
-    constructor(page: Page) {
-        super(page);
+  constructor(page: Page) {
+    super(page);
 
-        this.userNameLocator = this.page.locator('#username');
-        this.passwordLocator = this.page.locator('#password');
-        this.loginFormLocator = this.page.locator('#login');
-        this.loginTextContentLocator = this.page.locator('h4.subheader');
-    }
+    this.userNameLocator = this.page.locator("#username");
+    this.passwordLocator = this.page.locator("#password");
+    this.loginFormLocator = this.page.locator("#login");
+    this.loginTextContentLocator = this.page.locator("h4.subheader");
+  }
 
-    async getUsernameAndPassword(): Promise<[string, string]> {
-        const nameAndPassword: string[] = await this.page.locator('h4.subheader > em').allInnerTexts();
-        const username = nameAndPassword[0];
-        const password = nameAndPassword[1];
-        return [username, password];
-    }
+  async getUsernameAndPassword(): Promise<[string, string]> {
+    return await test.step("Read credential tips from the form", async () => {
+      const nameAndPassword: string[] = await this.page
+        .locator("h4.subheader > em")
+        .allInnerTexts();
+      const username = nameAndPassword[0];
+      const password = nameAndPassword[1];
+      return [username, password];
+    });
+  }
 
-    async fillUsernameAndPassword(username: string, password: string): Promise<void> {
-        await this.userNameLocator.fill(username);
-        await this.passwordLocator.fill(password);
-    }
+  async fillUsernameAndPassword(
+    username: string,
+    password: string
+  ): Promise<void> {
+    await this.userNameLocator.fill(username);
+    await this.passwordLocator.fill(password);
+  }
 
-    async performLoginActions(username: string, password: string): Promise<void> {
-        await this.fillUsernameAndPassword(username, password);  
-        await this.page.click('button[type="submit"]');
-    }
+  async performLoginActions(username: string, password: string): Promise<void> {
+    await test.step("Login to application", async () => {
+      await this.fillUsernameAndPassword(username, password);
+      await this.page.click('button[type="submit"]');
+    });
+  }
 
-    async getWelcomeMessage(): Promise<string> {
-        return await this.page.textContent('h4.subheader') as string;
-    }
+  async getWelcomeMessage(): Promise<string> {
+    return (await this.page.textContent("h4.subheader")) as string;
+  }
 
-    async isLogoutButtonDisplayed(): Promise<boolean> {
-        return await this.page.isVisible('a >> text="Logout"');
-    }
+  async isLogoutButtonDisplayed(): Promise<boolean> {
+    return await this.page.isVisible('a >> text="Logout"');
+  }
 
-    async clickLogoutButton(): Promise<void> {
-        await this.page.click('a >> text="Logout"');
-    }
+  async clickLogoutButton(): Promise<void> {
+    await this.page.click('a >> text="Logout"');
+  }
+
+  async verifyThatUserWasLoggedIn(): Promise<void> {
+    await test.step("Verify that user was logged in", async () => {
+      expect(await this.getWelcomeMessage()).toBe(
+        "Welcome to the Secure Area. When you are done click logout below."
+      );
+      expect(await this.isLogoutButtonDisplayed()).toBeTruthy();
+    });
+  }
 }
--- _project-herokuapp-components-tests/tests/login-page.tests.ts
+++ _project-herokuapp-components-tests/tests/login-page.tests.ts
@@ -1,21 +1,60 @@
-import { test, expect } from "../fixture-extention";
+import { test } from "../fixture-extention";
 import PageDataConstants from "../pages/pages-constants";
+const { wrap, configure } = require("agentql");
+const { chromium } = require("playwright");
 
-test.describe.parallel('Form-authentication page tests - Verify page logic', async () => {
-    test.beforeEach(async ({ mainPage, baseURL }) => {
-        await mainPage.navigateUrl(new PageDataConstants(baseURL as string).loginPage.getUrlPath());
-        await mainPage.waitForPageLoad();
+test.describe
+  .parallel("Form-authentication page tests - Verify page logic", async () => {
+  test.beforeEach(async ({ mainPage, baseURL }) => {
+    await test.step("Go to Login page", async () => {
+      await mainPage.navigateUrl(
+        new PageDataConstants(baseURL as string).loginPage.getUrlPath()
+      );
+      await mainPage.waitForPageLoad();
     });
+  });
 
-    test('Perform authentication with provided credentials on page', async ({ loginPage: formAuthenticationPage }) => {
-        //Arrange
-        const [username, password] = await formAuthenticationPage.getUsernameAndPassword();
+  test("Perform authentication with provided credentials on page", async ({
+    loginPage: formAuthenticationPage,
+  }) => {
+    //Arrange
+    const [username, password] =
+      await formAuthenticationPage.getUsernameAndPassword();
 
-        //Act
-        await formAuthenticationPage.performLoginActions(username, password);
-        
-        //Assert
-        expect(await formAuthenticationPage.getWelcomeMessage()).toBe('Welcome to the Secure Area. When you are done click logout below.');
-        expect(await formAuthenticationPage.isLogoutButtonDisplayed()).toBeTruthy();
-    });  
+    //Act
+    await formAuthenticationPage.performLoginActions(username, password);
+
+    //Assert
+    await formAuthenticationPage.verifyThatUserWasLoggedIn();
+  });
+
+  test.skip("Test example using AgentQL", async ({
+    loginPage: formAuthenticationPage,
+  }) => {
+    //Arrange
+    const browser = await chromium.launch({ headless: false });
+    const page = await wrap(await browser.newPage());
+    await page.goto("https://the-internet.herokuapp.com/login");
+    const username = "";
+    const password = "";
+    const searchButton = await page.getByPrompt("submit button");
+
+    const QUERY = `
+      {
+          login {
+              username
+              password
+          }
+      }`;
+
+    const response = await page.queryElements(QUERY);
+
+    //Act
+    await response.login.username.fill(username);
+    await response.login.password.fill(password);
+    await searchButton.click();
+
+    //Assert
+    await formAuthenticationPage.verifyThatUserWasLoggedIn();
+  });
 });
